fe0540103509717671176b539afed395ee2469d0
Almazov Urmat <almazov.urmat@gmail.com>
2023-07-01 12:44:08 +0600

    created Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
 import './App.css';
+import MainPage from "./containers/MainPage/MainPage";
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path={"/"} element={(
+            <MainPage />
+        )} />
+      </Routes>
     </div>
   );
 }
