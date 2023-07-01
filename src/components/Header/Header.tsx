74e1e00e067ad5bbaf602f9e61d339672b2fc67a
Almazov Urmat <almazov.urmat@gmail.com>
2023-07-01 17:14:59 +0600

    changed text in Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
         <header className="py-5 bg-light border-bottom mb-4">
             <div className="container">
                 <div className="text-center my-5">
-                    <h1 className="fw-bolder">Welcome to Blog Home!</h1>
-                    <p className="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
+                    <h1 className="fw-bolder">Добро пожаловать Quotes Book!</h1>
+                    <p className="lead mb-0">Здесь вы найдете цитаты на все случаи жизни</p>
                 </div>
             </div>
         </header>
