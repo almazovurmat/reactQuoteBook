9e09451167bc08c9aa659e56f6b43cf4dc5b84c8
Almazov Urmat <almazov.urmat@gmail.com>
2023-07-01 18:14:08 +0600

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Outlet, Route, Routes} from "react-router-dom";
 import './App.css';
 import MainPage from "./containers/MainPage/MainPage";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import AddQuote from "./containers/AddQuote/AddQuote";
+import QuoteEdit from "./containers/Quotes/QuoteEdit";
+import QuoteDelete from "./containers/Quotes/QuoteDelete";
+import QuotesByCategory from "./containers/QuotesByCategory/QuotesByCategory";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path={"/"} element={(
-            <MainPage />
-        )} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar/>
+            <Header/>
+            <Routes>
+                <Route path={"/"} element={(
+                    <MainPage/>
+                )}/>
+
+                <Route path={"/add-quote"} element={(
+                    <AddQuote/>
+                )}/>
+
+                <Route path="/quotes" element={<Outlet />} >
+                    <Route path="/quotes" element={<MainPage />} />
+                    <Route path="/quotes/:id/edit" element={<QuoteEdit />} />
+                    <Route path="/quotes/:id/delete" element={<QuoteDelete />} />
+                    <Route path="/quotes/:category" element={<QuotesByCategory />} />
+                </Route>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
