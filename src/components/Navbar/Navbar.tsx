da3c26bac4d8d85271e11ea06efe61c4116b11ce
Almazov Urmat <almazov.urmat@gmail.com>
2023-07-01 17:15:56 +0600

    added link to add new quote

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item"><NavLink className="nav-link" to={'/'}>Home</NavLink></li>
                         <li className="nav-item"><NavLink className="nav-link" to={'/'}>Quotes</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to={'/'}>Add New Quotes</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to={'/add-quote'}>Add New Quotes</NavLink></li>
                     </ul>
                 </div>
             </div>
