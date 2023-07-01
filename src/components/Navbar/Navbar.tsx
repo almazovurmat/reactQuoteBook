b0f6cc239a14865987934bab6e523d53a7512768
Almazov Urmat <almazov.urmat@gmail.com>
2023-07-01 12:47:45 +0600

    created NavBar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link, NavLink} from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <Link className="navbar-brand" to={'/'}>Quotes Book</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation"><span
+                    className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item"><NavLink className="nav-link" to={'/'}>Home</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to={'/'}>Quotes</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to={'/'}>Add New Quotes</NavLink></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
